509cb31a2a17075737a06567de21adc1f5f516dd
Daniel Ruiz Barboza <daniel.r.b.d22@gmail.com>
2022-08-08 17:51:32 -0300

    Importação Inicial

---
+++ main.py
@@ -0,0 +1,16 @@
+from fila_normal import FilaNormal
+from fila_prioritaria import FilaPrioritaria
+
+# fila_teste = FilaNormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+# print(fila_teste.chamacliente(10))
+
+fila_teste2 = FilaPrioritaria()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+print(fila_teste2.chama_cliente(10))
+print(fila_teste2.chama_cliente(1))
+print(fila_teste2.estatistica("08/08/2022", 123, "detail"))

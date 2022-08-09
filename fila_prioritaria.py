b5b1e8310a4c594b4232a04d52ce97a4894e5cb5
Daniel Ruiz Barboza <daniel.r.b.d22@gmail.com>
2022-08-09 16:40:42 -0300

    Criando Fila Base

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,24 +1,17 @@
-class FilaPrioritaria:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ""
+from fila_base import FilaBase
+
+
+class FilaPrioritaria(FilaBase):
 
     def gera_senha_atual(self) -> None:
         self.senha_atual = f"PR{self.codigo}"
-
-    def reseta_fila(self)-> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
 
     def atualiza_fila(self) -> None:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
 
-    def chama_cliente(self, caixa: int) -> None:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f"Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}"
@@ -27,6 +20,10 @@ class FilaPrioritaria:
         if flag != "detail":
             estatistica = {f"{agencia}-{dia}": len(self.clientes_atendidos)}
         else:
-            estatistica = {"Dia": dia, "Agência": agencia, "Clientes Atendidos": self.clientes_atendidos,
-                           "Quantidade de Clientes Atendidos": len(self.clientes_atendidos)}
+            estatistica = {"Dia": dia, "Agência": agencia,
+                           "Clientes Atendidos": self.clientes_atendidos,
+                           "Quantidade de Clientes Atendidos": len(
+                               self.clientes_atendidos
+                           )
+                           }
         return estatistica
